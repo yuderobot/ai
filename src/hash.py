ff6ec42f414251209c9dc2069e4e25ced66ffa45
yude <i@yude.jp>
2022-11-30 06:37:17 +0000

    fix: error handling @ hash

--- src/hash.py
+++ src/hash.py
@@ -1,8 +1,13 @@
 import git
 
 def get_hash():
-    repo = git.Repo(search_parent_directories=True)
-    git_hash = repo.git.rev_parse(repo.head.object.hexsha, short=6)
+    try:
+        repo = git.Repo(search_parent_directories=True)
+        git_hash = repo.git.rev_parse(repo.head.object.hexsha, short=6)
+    except:
+        git_hash = "N/A"
+
     if git_hash is None:
         git_hash = "N/A"
+
     return git_hash
